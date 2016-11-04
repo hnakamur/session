e01a2583b4ef97ff66b9ee82246212925bef1fb4
Hiroaki Nakamura <hnakamur@gmail.com>
2016-11-04 17:02:59 +0900

    Use redis connection pool

--- redis.go
+++ redis.go
@@ -10,21 +10,45 @@ import (
 )
 
 type redisStore struct {
-	conn redis.Conn
+	pool *redis.Pool
 }
 
 func NewRedisStore(address string) (Store, error) {
-	conn, err := redis.Dial("tcp", address)
-	if err != nil {
-		return nil, err
-	}
 	return &redisStore{
-		conn: conn,
+		pool: newRedisPool(address, ""),
 	}, nil
 }
 
+func newRedisPool(address, password string) *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", address)
+			if err != nil {
+				return nil, err
+			}
+			if password != "" {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
 func (s *redisStore) Get(ctx context.Context, id, key string, valuePtr interface{}) error {
-	reply, err := s.conn.Do("HGET", s.formatIDKey(id), key)
+	conn := s.pool.Get()
+	reply, err := conn.Do("HGET", s.formatIDKey(id), key)
 	if err != nil {
 		return err
 	}
@@ -42,28 +66,32 @@ func (s *redisStore) Set(ctx context.Context, id, key string, value interface{})
 	if err != nil {
 		return err
 	}
-	_, err = s.conn.Do("HSET", s.formatIDKey(id), key, v)
+	conn := s.pool.Get()
+	_, err = conn.Do("HSET", s.formatIDKey(id), key, v)
 	log.Printf("Set. id=%s, key=%s, err=%+v", id, key, err)
 	return err
 }
 
 func (s *redisStore) Remove(ctx context.Context, id, key string) error {
-	_, err := s.conn.Do("HDEL", s.formatIDKey(id), key)
+	conn := s.pool.Get()
+	_, err := conn.Do("HDEL", s.formatIDKey(id), key)
 	return err
 }
 
 func (s *redisStore) RemoveAll(ctx context.Context, id string) error {
-	_, err := s.conn.Do("DEL", s.formatIDKey(id))
+	conn := s.pool.Get()
+	_, err := conn.Do("DEL", s.formatIDKey(id))
 	return err
 }
 
 func (s *redisStore) Expire(ctx context.Context, id string, d time.Duration) error {
-	_, err := s.conn.Do("PEXPIRE", s.formatIDKey(id), int64(d/time.Millisecond))
+	conn := s.pool.Get()
+	_, err := conn.Do("PEXPIRE", s.formatIDKey(id), int64(d/time.Millisecond))
 	return err
 }
 
 func (s *redisStore) Close() error {
-	return s.conn.Close()
+	return s.pool.Close()
 }
 
 func (s *redisStore) formatIDKey(id string) string {
